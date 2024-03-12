ab5bb186293c0b1c5f9c0d5529f15d82de0e3465
Gabriel <gzanella202@gmail.com>
2024-03-11 23:27:05 -0300

    Exercicio02

---
+++ Exercicio02.java
@@ -0,0 +1,11 @@
+public class Exercicio02 {
+    public static void executar(){
+
+        double numero1 = Prompt.lerDecimal("Digite o primeiro número:");
+        double numero2 = Prompt.lerDecimal("Digite o segundo número:");
+        double soma = numero1 + numero2;
+
+        Prompt.imprimir("A soma é: " + soma);
+
+    }
+}
